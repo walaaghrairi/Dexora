6bc8878a231e5cb420f2e4de1114cfdca76b18d2
walaaghrairi <walaaeddine.ghrairi@esprit.tn>
2026-08-07 08:56:28 +0100

    feat: add User entity

---
+++ backend/src/main/java/com/dexora/entity/User.java
@@ -0,0 +1,59 @@
+package com.dexora.entity;
+
+import com.dexora.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String hashPassword;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ backend/src/main/java/com/dexora/enums/Role.java
@@ -0,0 +1,7 @@
+package com.dexora.enums;
+
+public enum Role {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
